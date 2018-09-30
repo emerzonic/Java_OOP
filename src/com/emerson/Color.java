586767895d5ce4b788b294b65457fc86dcf067b0
Emerson Doyah <emerson.doyah@gmail.com>
2018-09-30 11:02:31 -0500

    refactor color

--- src/com/emerson/Color.java
+++ src/com/emerson/Color.java
@@ -1,9 +1,9 @@
 package com.emerson;
 
 public class Color {
-    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String reset = "\u001B[0m";
 
-    public static final String ANSI_RED = "\u001B[31m";
+    public static final String red = "\u001B[31m";
 
-    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String green = "\u001B[32m";
 }

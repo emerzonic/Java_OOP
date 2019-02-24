d16984b67c57a860779aa220f4d350af6538c1b1
Emerson Doyah <emerson.doyah@gmail.com>
2019-02-23 20:35:17 -0600

    refactor checkGuess method in Letter class

--- src/com/emerson/Letter.java
+++ src/com/emerson/Letter.java
@@ -1,33 +1,27 @@
 package com.emerson;
 
- class Letter {
-    private String  letter;
-    private String placeHolder;
-    private boolean status;
+class Letter {
+  private String letter;
+  private String placeHolder;
+  private boolean status;
 
-     Letter(String letter){
-        this.letter = letter;
-        this.placeHolder = "_";
-        this.status = false;
+  Letter(String letter) {
+    this.letter = letter;
+    this.placeHolder = "_";
+    this.status = false;
+  }
+
+
+  //This method checks every letter in the word and returns a letter or a placeholder underscore
+  String checkGuess() {
+    return status ? letter : placeHolder;
+  }
+
+
+  //This method takes a letter as guess and checks it against each letter of the word
+  void takeGuess(String guess) {
+    if (this.letter.equals(guess)) {
+      this.status = true;
     }
-
-
-    //This method checks every letter in the word and returns a letter or a placeholder underscore
-     String checkGuess(){
-       String character = "";
-        if(this.status){
-          character = letter;
-        }else {
-          character = placeHolder;
-        }
-        return character;
-    }
-
-
-    //This method takes a letter as guess and checks it against each letter of the word
-     void takeGuess(String guess){
-        if(this.letter.equals(guess)){
-            this.status = true;
-        }
-    }
+  }
 }
